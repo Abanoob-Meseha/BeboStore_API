e03b70cc5bec3a8adca09f694ad2e56306fce6c8
unknown <abanoob.meseha@gmail.com>
2025-03-19 22:05:11 +0200

    Added: Delete product by id endpoint Tested

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,16 +1,16 @@
 const { AppError } = require("../middlewares/errorHandling");
 const { Product } = require("../models");
-const { uploadToCloudinary} = require("../config/cloudinary");
+const { uploadToCloudinary } = require("../config/cloudinary");
 
 // add product and return product Object
 const addProduct = async (req, res, next) => {
   try {
-    const productExists = await Product.findOne({name: req.body.name});
-    if(!!productExists){
+    const productExists = await Product.findOne({ name: req.body.name });
+    if (!!productExists) {
       return res.status(400).json({
-        success: false ,
-        message : "Product name Already Exixts"
-      })
+        success: false,
+        message: "Product name Already Exixts",
+      });
     }
     if (!req.files || req.files.length === 0) {
       return res
@@ -19,7 +19,7 @@ const addProduct = async (req, res, next) => {
     }
     // Upload each image to Cloudinary
     const imageUploadPromises = req.files.map((file) =>
-      uploadToCloudinary(file.buffer , "Products")
+      uploadToCloudinary(file.buffer, "Products")
     );
     const imageUrls = await Promise.all(imageUploadPromises);
     let product = new Product({ ...req.body, imgs: [...imageUrls] });
@@ -82,6 +82,25 @@ const getProductByID = async (req, res, next) => {
     next(error);
   }
 };
+
+// delete product by ID
+const deleteProduct = async (req, res, next) => {
+  try {
+    let productID = req.params.id;
+    let product = await Product.findByIdAndDelete(productID);
+    if (!product) {
+      throw new AppError(404, "Product Delete Failed .. or not Found");
+    }
+    res.status(200).json({
+      status: "success",
+      message: "Product Deleted Successfully .. Done",
+      data: {},
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 // Update Product By Id
 const updateProduct = async (req, res, next) => {
   try {
@@ -99,23 +118,6 @@ const updateProduct = async (req, res, next) => {
       data: {
         product: product,
       },
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-// delete product by ID
-const deleteProduct = async (req, res, next) => {
-  try {
-    let productID = req.params.id;
-    let product = await Product.findByIdAndDelete(productID);
-    if (!product) {
-      throw new AppError(404, "Product Delete Failed .. or not Found");
-    }
-    res.status(200).json({
-      status: "success",
-      message: "Product Deleted Successfully .. Done",
-      data: {},
     });
   } catch (error) {
     next(error);
