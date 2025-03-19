c1c3df0bfcb1689b3c99b836b28d4ef70a5c6ccc
unknown <abanoob.meseha@gmail.com>
2025-03-19 23:08:35 +0200

    Added: cors for cross origin resource sharing with front-end App on port 3000

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-require('dotenv').config(); // config .env file vars
-const express = require('express');
+require("dotenv").config(); // config .env file vars
+const express = require("express");
 const app = express();
-const productsRouter = require('./routes/productsRoutes')
-const authRouter = require('./routes/authRoutes')
-const cartRouter = require('./routes/cartRoutes')
-const ordersRouter = require('./routes/ordersRoutes')
-const paymentsRouter = require('./routes/paymentsRoutes')
-const errorHandling = require('./middlewares/errorHandling'); 
-const cookieParser = require('cookie-parser');
+const productsRouter = require("./routes/productsRoutes");
+const authRouter = require("./routes/authRoutes");
+const cartRouter = require("./routes/cartRoutes");
+const ordersRouter = require("./routes/ordersRoutes");
+const paymentsRouter = require("./routes/paymentsRoutes");
+const errorHandling = require("./middlewares/errorHandling");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+// Allow to share resources with Front-End App
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 // for parsing req cookie to js object --> req.cookies
 app.use(cookieParser());
 // for parsing html forms to js object --> req.body
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 // parsing requests in json Format to js object --> req.body
 app.use(express.json());
 // for Adding Routers
-app.use('/auth', authRouter)
-app.use('/products' , productsRouter)
-app.use('/orders' , ordersRouter)
-app.use('/cart' , cartRouter)
-app.use('/payments' , paymentsRouter)
+app.use("/auth", authRouter);
+app.use("/products", productsRouter);
+app.use("/orders", ordersRouter);
+app.use("/cart", cartRouter);
+app.use("/payments", paymentsRouter);
 
 // i protected this route using jwt token check auth
-app.get('/'  ,(req , res , next)=>{
-    try{
-        res.status(200).json({
-            message: `Welcome to BeboStore API`,
-            status: 200
-        })
-    }catch(err){
-        next(err)
-    }
-    
-})
+app.get("/", (req, res, next) => {
+  try {
+    res.status(200).json({
+      message: `Welcome to BeboStore API`,
+      status: 200,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 // we put it at the End to Pass Error to it after Proccessing on Router Endpoints
-app.use(errorHandling)
+app.use(errorHandling);
 
-
-module.exports = app ;
+module.exports = app;
